297328db5e7a1330ec9f6306c3dba61b6c40a192
rmad17 <souravbasu17@gmail.com>
2016-04-13 22:31:29 +0530

    Added static file whitenoise

--- todolist/settings.py
+++ todolist/settings.py
@@ -123,5 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 #STATIC_URL = '/static/'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
